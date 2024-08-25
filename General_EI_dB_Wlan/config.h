3f47e055800495652da3e00bd3cb88e8b1689ee0
Anni K. <bun.ny+github@hotmail.com>
2024-08-25 21:37:38 +0200

    🤦

--- General_EI_dB_Wlan/config.h
+++ General_EI_dB_Wlan/config.h
@@ -10,15 +10,15 @@
 #define I2S_SD SDA
 
 // WiFi Configuration
-#define WIFI_SSID "FRITZ!Box 7530 PU"
-#define WIFI_PASSWORD "83383527870040966533"
+#define WIFI_SSID "WiFi-Network-Name"           // WiFi that the device should connect to
+#define WIFI_PASSWORD "WiFi-Network-Password"   // WiFi Password
 
 // API Configuration
-#define API_URL "http://192.168.178.69:5000/update"
-#define AUTH_BEARER "Bearer test123"
+#define API_URL "https://backend.com/update"    // POST address of the TinyAIoT Backend
+#define AUTH_BEARER "Bearer Secure-Secret-Key"  // Secure API Token
 
 // Project and Sensor IDs
-#define PROJECT_ID "Project123"
-#define SENSOR_ID "SensorABC"
+#define PROJECT_ID "Project-Id"     // Project of the device
+#define SENSOR_ID "Sensor-Id"       // Identification of one specific device
 
 #endif // CONFIG_H
